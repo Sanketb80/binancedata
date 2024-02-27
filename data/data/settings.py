edd9f55a801ec438cbd3c683112b4b00ec10e461
sanket <sanketb81@gmail.com>
2024-02-27 11:18:02 +0530

    fdf

--- data/data/settings.py
+++ data/data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-09mf!tpy)tc$n0ih*_5gmy72-lil#vxgub1$e-s$g=99p9+0(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.9','localhost']
 
 
 # Application definition
